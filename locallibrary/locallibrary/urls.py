d1e3b1056aaafafa78049383725ee381a95ec3e2
chocolatito <sal_defrutas@hotmail.com>
2020-01-07 13:46:04 -0300

    configuración del URL_CONF raíz
    la url '/' se redirecciona a la url '/catalog/'

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    # al usar el parametro '/' en lugar de ''
+    # se requiere la funcion static()
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
