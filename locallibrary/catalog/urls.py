f2a2ce0806ff0c54d3152875a7d27c4938c39a31
chocolatito <sal_defrutas@hotmail.com>
2020-01-13 14:11:33 -0300

    Creación de pagina de inicio y uso de vistas genericas
    listViews y detailViews

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^Books/$',
+        views.BookListView.as_view(),
+        name='books'),
+    url(r'^book/(?P<pk>\d+)$',
+        views.BookDetailView.as_view(),
+        name='book-detail'),
+
 
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
@@ -27,3 +27,33 @@ def index(request):
                  'num_instances_available': num_instances_available,
                  'num_authors': num_authors},
     )
+
+
+# Nueva vista
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+    # your own name for the list as a template variable
+    # context_object_name = 'my_book_list'
+    # Get 5 books containing the title war
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    # Specify your own template name/location
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+
+    # Get 5 books containing the title war
+    def get_queryset(self):
+        return Book.objects.all()  # filter(title__icontains='autor1')[:5]
+
+    # Call the base implementation first to get a context
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Get the blog from id and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+# https://developer.mozilla.org/es/docs/Learn/Server-side/Django/Sessions
